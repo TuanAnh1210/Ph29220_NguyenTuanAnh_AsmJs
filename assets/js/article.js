53e52258d326665de177f26e3832c425697b21a2
TuanAnhNguyen <tuananh1210085213@gmail.com>
2022-08-08 11:08:37 +0700

    add render article

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,3 +1,5 @@
+// import { itemsArticle } from "./itemsArticle.js";
+
 // show hide create - article
 
 const modalArticle = document.querySelector(".modal__article-wrapper");
@@ -7,11 +9,16 @@ const modal__article = document.querySelector(".modal__article");
 
 btnCreate.onclick = () => {
   modalArticle.classList.add("open");
+  document.querySelector("body").style.overflow = "hidden";
 };
 
 btnArticleClose.onclick = (e) => {
   e.preventDefault();
   modalArticle.classList.remove("open");
+  document.querySelector("body").style.overflow = "";
+  for (const field of formField) {
+    document.getElementById(field).parentElement.classList.remove("red");
+  }
 };
 
 modal__article.onclick = (e) => {
@@ -19,6 +26,10 @@ modal__article.onclick = (e) => {
 };
 modalArticle.onclick = () => {
   modalArticle.classList.remove("open");
+  for (const field of formField) {
+    document.getElementById(field).parentElement.classList.remove("red");
+  }
+  document.querySelector("body").style.overflow = "";
 };
 
 //
@@ -100,32 +111,125 @@ const articleItemsContainer = document.querySelector(
 );
 
 let temp = [];
-temp = itemsArticle.map(
-  (item) =>
-    `
-    <div class="col l-3">
-                <div class="article__item">
-                  <div class="article__item-img">
-                    <img  src="${item.image}" alt="">
-                  </div>
-  
-                 <div class="article__item-text">
-                   <div class="article__item-title"> 
-                    <h2 >${item.title}</h2>
-                  </div>
 
-                   <div class="article__item-decs"> 
-                    <p>${item.description}</p>
-                  </div>
-    
-                    <div class="article__item-author">
-                      <img src="./assets/imgs/author2.jpg" alt="">
-                      <p><strong>${item.composer}</strong> on Jul 28, 2022</p>
+function render() {
+  let articleRender = [...itemsArticle];
+  let articleMain = articleRender.reverse().slice(0, 8);
+  temp = articleMain.map(
+    (item) =>
+      `
+      <div class="col l-3">
+                  <div class="article__item">
+                    <div class="article__item-img">
+                      <img  src="${item.image}" alt="">
                     </div>
-                 </div>
+    
+                   <div class="article__item-text">
+                     <div class="article__item-title"> 
+                      <h2 >${item.title}</h2>
+                    </div>
+  
+                     <div class="article__item-decs"> 
+                      <p>${item.description}</p>
+                    </div>
+      
+                      <div class="article__item-author">
+                        <img src="./assets/imgs/author2.jpg" alt="">
+                        <p><strong>${item.composer}</strong> on Jul 28, 2022</p>
+                      </div>
+                   </div>
+                  </div>
                 </div>
-              </div>
-    `
-);
+      `
+  );
+  articleItemsContainer.innerHTML = temp.join("");
+}
 
-articleItemsContainer.innerHTML = temp.join("");
+// get form submit
+
+const btnArticleAdd = document.querySelector(".btn__article-add");
+const input__title = document.querySelector("#title");
+const input__img = document.querySelector("#image");
+const input__composer = document.querySelector("#composer");
+const input__email = document.querySelector("#email");
+const input__desc = document.querySelector("#description");
+
+const formField = ["title", "image", "composer", "email", "description"];
+
+btnArticleAdd.onclick = function (e) {
+  e.preventDefault();
+
+  let articleData = {};
+  validate(articleData);
+  if (Object.keys(articleData).length != 0) {
+    itemsArticle.push(articleData);
+    render();
+
+    clearInput();
+  }
+  console.log(itemsArticle);
+};
+
+// clear input
+
+function clearInput() {
+  input__title.value = "";
+  input__img.value = "";
+  input__composer.value = "";
+  input__email.value = "";
+  input__desc.value = "";
+}
+
+// Validate form
+
+function validate(articleData) {
+  let isError = true;
+  for (let field of formField) {
+    let element = document.getElementById(field);
+    const regEmail = /^\w+@(\w+\.\w+){1,2}$/;
+
+    if (field == "title") {
+      if (element.value.trim().length < 10) {
+        element.parentElement.classList.add("red");
+        element.parentElement.querySelector(".errror").innerText =
+          "Title phải lớn hơn 10 kí tự";
+        isError = false;
+      }
+    }
+
+    if (field == "email") {
+      if (regEmail.test(element.value.trim()) == false) {
+        element.parentElement.classList.add("red");
+        element.parentElement.querySelector(".errror").innerText =
+          "Email sai định dạng";
+        isError = false;
+      }
+    }
+
+    if (field == "description") {
+      if (element.value.trim().length < 20) {
+        element.parentElement.classList.add("red");
+        element.parentElement.querySelector(".errror").innerText =
+          "Description phải lớn hơn 20 kí tự";
+        isError = false;
+      }
+    }
+
+    if (element.value.trim() === "") {
+      element.parentElement.classList.add("red");
+      element.parentElement.querySelector(
+        ".errror"
+      ).innerText = `Vui lòng nhập ${
+        element.parentElement.querySelector("label").innerText
+      }`;
+      isError = false;
+    }
+
+    element.oninput = () => {
+      element.parentElement.classList.remove("red");
+    };
+    if (isError) {
+      articleData[field] = element.value;
+    }
+  }
+}
